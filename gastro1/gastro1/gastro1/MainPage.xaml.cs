6cbdb1c2707115a8e8923f246941bf977ac94835
jojiwesley <joji.wesley@hotmail.com>
2019-10-22 19:00:19 -0300

    GASTRO.1
    layout_login

---
+++ gastro1/gastro1/gastro1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gastro1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ gastro1/gastro1/gastro1/Modelos/SecondPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gastro1.Modelos
+{
+    class SecondPage
+    {
+        public string  fileName { get; set; }
+
+        public string text { get; set; }
+
+        public string DateTimeDate { get; set; }
+
+    }
+}
